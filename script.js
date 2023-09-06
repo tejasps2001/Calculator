54db9fa87f526f184f0389611482ba8821122b46
Tejas P S <tejasps2001@gmail.com>
2023-09-06 16:06:20 +0530

    Add updateDisplay function to reflect inputs in calc display

--- script.js
+++ script.js
@@ -4,3 +4,36 @@ let operators = {
     "+": (a, b) => a + b,
     "-": (a, b) => a - b,
 };
+
+let a = 0;
+let op = '';
+let b = 0;
+
+let operate = (a, op, b) => operators[op](a, b);
+let displayString = document.querySelector('#expression');
+
+function updateDisplay(e) {
+    displayString.textContent += e.target.innerText;
+}
+
+function getOperands() {
+    let expression = new Array(1);
+    let ops = Object.keys(operators);
+    for (op of ops) {
+        expression = displayString.textContent.split(op);
+        if (expression.length > 1) break;
+    }
+    console.log(expression)
+    return expression[0], expression[1], op;
+}
+
+let btns = document.querySelectorAll('button');
+btns.forEach(btn => btn.addEventListener('click', e => {
+    if (btn.innerText == '=') {
+        let a = getOperands();
+        console.log(a)
+        let result = operate(a, op, b);
+        updateResult(result);
+    }
+    updateDisplay(e);
+}))
